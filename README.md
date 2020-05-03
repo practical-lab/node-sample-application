4cc98734b6c948900643e4a0fc14fe6ee46c7e3f
Shoji Kunitomo <shoji.kunitomo@gmail.com>
2020-05-03 09:37:30 +0000

    Added some packages and deleted build directory not to trace.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # node-hello
+
+This project is based on the following tools.
+
+- docker
+- node: v12.16.1
+- Type Script: v3.8.3
+- VS Code
--- build/js/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var hello = function (name) {
-    return console.log('Hello', name);
-};
-hello('You');
-//# sourceMappingURL=index.js.map
